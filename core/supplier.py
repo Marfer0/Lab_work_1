f9e85164cb130a75a55d5301843e992cef1c874c
Marfer0 <vadimka.marunov@gmail.com>
2025-11-26 01:13:33 +0300

    Добавлены функции обновления и удаления, а также функция поставки товара на склад и вывода данных

--- core/supplier.py
+++ core/supplier.py
@@ -8,3 +8,19 @@ class Supplier:
             "id": self.supplier_id,
             "название": self.name
         }
+    def update_name(self, new_name):
+        self.name = new_name
+
+    def supply_item(self, warehouse, item):
+        try:
+            warehouse.add_item(item)
+        except Exception as e:
+            print(f"Ошибка поставки {item.name} на склад {warehouse.name}: {e}")
+
+
+    def delete(self):
+        self.supplier_id = None
+        self.name = None
+
+    def __repr__(self):
+        return f"Поставщик: {self.name}"
