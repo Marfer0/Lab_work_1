b6975be4dc9052b948eac3cdc373c21afbac2f06
Marfer0 <vadimka.marunov@gmail.com>
2025-11-26 01:08:44 +0300

    Добавлены функции добавления и удаления

--- core/structure.py
+++ core/structure.py
@@ -8,3 +8,19 @@ class Structure:
             "склады": {wid: wh.get_info() for wid, wh in self.warehouses.items()},
             "категории": {cid: cat.get_info() for cid, cat in self.categories.items()}
         }
+
+    def add_warehouse(self, warehouse):
+        self.warehouses[warehouse.warehouse_id] = warehouse
+
+    def remove_warehouse(self, warehouse_id):
+        return self.warehouses.pop(warehouse_id, None)
+
+    def add_category(self, category):
+        self.categories[category.category_id] = category
+
+    def remove_category(self, category_id):
+        return self.categories.pop(category_id, None)
+
+    def delete(self):
+        self.warehouses = {}
+        self.categories = {}
