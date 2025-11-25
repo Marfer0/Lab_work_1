3322acd2e7a83bdaa51c1b330f26f9289374bf74
Marfer0 <vadimka.marunov@gmail.com>
2025-11-26 01:32:20 +0300

    Добавлены функции добавления, удаления и получения элемента по id в секции.

--- core/section.py
+++ core/section.py
@@ -12,3 +12,20 @@ class Section:
             "вместимость": self.capacity,
             "товары": [item.get_info() for item in self.items]
         }
+    def add_item(self, item):
+        total_qty = sum(i.quantity for i in self.items)
+        if total_qty + item.quantity > self.capacity:
+            raise ValueError(f"Невозможно добавить {item.name}, секция '{self.name}' переполнена")
+        self.items.append(item)
+
+    def remove_item(self, item_id):
+        for i, item in enumerate(self.items):
+            if item.item_id == item_id:
+                return self.items.pop(i)
+        raise ValueError(f"Товар с id {item_id} не найден в секции '{self.name}'")
+
+    def get_item(self, item_id):
+        for item in self.items:
+            if item.item_id == item_id:
+                return item
+        raise ValueError(f"Товар с id {item_id} не найден в секции '{self.name}'")
