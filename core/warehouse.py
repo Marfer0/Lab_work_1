aed3bf8c7d5e734902e8143b5df0b53327d42136
Marfer0 <vadimka.marunov@gmail.com>
2025-11-26 00:49:29 +0300

    Добавлены функции удаления и добавления, а также получения по айди склада

--- core/warehouse.py
+++ core/warehouse.py
@@ -15,3 +15,20 @@ class Warehouse:
             "вместимость": self.capacity,
             "товары": [item.get_info() for item in self.items]
         }
+    def add_item(self, item: Item):
+        total_qty = sum(i.quantity for i in self.items)
+        if total_qty + item.quantity > self.capacity:
+            raise WarehouseFullError(f"Невозможно добавить {item.name}, склад '{self.name}' переполнен")
+        self.items.append(item)
+
+    def remove_item(self, item_id):
+        for i, item in enumerate(self.items):
+            if item.item_id == item_id:
+                return self.items.pop(i)
+        raise ItemNotFoundError(f"Товар с id {item_id} не найден на складе '{self.name}'")
+
+    def get_item(self, item_id):
+        for item in self.items:
+            if item.item_id == item_id:
+                return item
+        raise ItemNotFoundError(f"Товар с id {item_id} не найден на складе '{self.name}'")
